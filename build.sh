7b774e83d174cff471a6163738f0d7bd311b761a
Nicolas Busser <nicolas.busser@lacollection.io>
2024-04-30 15:25:03 +0900

    fix(add): missing shebang

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build
--- clean.sh
+++ clean.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 rm -rf build 2>/dev/null
 rm -rf CMakeFiles 2>/dev/null
