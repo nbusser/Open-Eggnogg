ef9c562acef45b0c7ec4b70a897fdff54289099c
Nicolas Busser <nicolas.busser@lacollection.io>
2024-04-30 14:49:49 +0900

    fix: format

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,17 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+int main() {
+  auto window = sf::RenderWindow{{1920u, 1080u}, "CMake SFML Project"};
+  window.setFramerateLimit(60);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.display();
+  }
 }
