0b29a866c75eb7baff4a61cd9f7f3eb4e815d7fe
Nicolas Busser <nicolas.busser@lacollection.io>
2024-05-01 15:48:18 +0900

    feat(add): list of physics body in game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include "Character.hpp"
+#include "PhysicsBody.hpp"
 #include "SFML/System/Vector2.hpp"
 #include <SFML/Graphics.hpp>
+#include <memory>
+#include <vector>
 
 int main() {
-  Character player = Character(sf::Vector2f(0.0, 0.0));
+  auto player1 =
+      std::make_shared<Character>(Character(sf::Vector2f(-15.0, 0.0)));
+  auto player2 =
+      std::make_shared<Character>(Character(sf::Vector2f(15.0, 0.0)));
+
+  std::vector<std::shared_ptr<PhysicsBody>> physicsBodies{player1, player2};
 
   auto window = sf::RenderWindow{{1920u, 1080u}, "Open Eggnogg"};
   window.setFramerateLimit(60);
@@ -13,6 +21,10 @@ int main() {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
+    }
+
+    for (const auto &physicsBodyPtr : physicsBodies) {
+      (*physicsBodyPtr).physicsTick();
     }
 
     window.clear();
