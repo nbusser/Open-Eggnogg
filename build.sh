eae568063826ea11870762653bd38743ced340f2
Nicolas Busser <nicolas.busser@lacollection.io>
2024-05-01 16:05:14 +0900

    feat(add): build script options

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+MODE=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+if [ "${MODE}" = "release" ]; then
+  MODE=Release
+elif [ "${MODE}" = "debug" ]; then
+  MODE=Debug
+else
+  echo "Usage: $0 [release|debug]"
+  exit 1
+fi
+
 set -e
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -S . -B build -DCMAKE_BUILD_TYPE=${MODE} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build
 ln -sf assets build/bin/assets
